de35f8170d448c34737a9b17da6f1feabccc6cb9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-02 11:02:52 +0100

    Test

--- src/testing/TestMain.java
+++ src/testing/TestMain.java
@@ -7,8 +7,20 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Datevalide dt=new Datevalide(12, 3, 2013);
+Datevalide dt=new Datevalide(29, 2, 2012);
 ValidationDate validation=new ValidationDate();
+ boolean verification=validation.validJour(dt.getJour(), dt.getMois(), dt.getAn());
+if(verification) 
+{
+System.out.println(dt.toString()+" est valide");
+
+}
+System.out.println(verification);
+
+if(validation.validJour(dt.getJour(), dt.getMois(), dt.getAn()) ==false)
+System.out.println(dt.toString()+" n'est pas valide");
+
 	}
 
 }
+
