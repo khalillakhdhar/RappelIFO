9f70239613b5b212d5b7aa68d361af2eedbb7d75
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-02 10:18:44 +0100

    validdateTesting

--- src/testing/TestVerif.java
+++ src/testing/TestVerif.java
@@ -1,5 +1,27 @@
 package testing;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import validation.ValidationDate;
 
 public class TestVerif {
+ValidationDate valide;
+@Before
+public void initialise()
+{
+valide=new ValidationDate();
 
+}
+@Test
+public void verifan()
+{
+assertTrue(valide.bissextile(2020));	
+}
+@Test
+public void verifierdate()
+{
+assertTrue(valide.validJour(29, 2, 2003));	
+}
 }
